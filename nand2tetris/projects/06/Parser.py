c0494d7a858b5d5e7c4206c73828ee03dcbf0a51
zs144 <zionsheng@yahoo.com>
2023-07-19 01:50:14 -0400

    Delete Parser module

--- nand2tetris/projects/06/Parser.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Parser:
-    def __init__(self, filename) -> None:
-        path = os.path.dirname(__file__)
-        filename = os.path.join(path, filename)
-        self.f
-        f = open(filename, 'r')
